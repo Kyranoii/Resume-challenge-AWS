a014f24d6e351cac590f6a4e79cf715add34e9ad
kyran idowu <kyran.idowu@hva.nl>
2026-02-20 00:10:26 +0100

    Index.html layout done, removed navbar.html and navbar.js, added navbar.ts

--- navbar.js
+++
@@ -1,9 +0,0 @@
-fetch("navbar.html")
-    .then(res => res.text())
-    .then(html => { const navbar = document.getElementById("navbar");
-        if (navbar){ 
-            navbar.innerHTML = html;
-        } else {
-            console.error("Element with id 'navbar' not found" )
-        }
-    });
