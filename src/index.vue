82ccbf0847e9ccb956d2f0ac1daa54a5b35d841e
kyran idowu <kyran.idowu@hva.nl>
2026-02-20 11:06:45 +0100

    Installed Vue. Removed unused files. Added body.vue. Added index.vue. Added nav.vue. Removed code inside index.html. Added vue referecing inside index.html.

---
+++ src/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <navBar/>
+  <Body/>
+</template>
+
+<script lang="ts">
+import navBar from './nav.vue'
+import Body from './body.vue'
+export default {
+  components: { navBar, Body }
+}
+</script>
---
+++ src/nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar">
+    <div class="navContainer">
+      <a class="home" href="#home"><h1 id="name">Kyran Idowu</h1></a>
+      <ul class="navList">
+        <li v-for="link in links" class="navItem">
+          <a class="navLink" href="#">{{ link }}</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: ['Work history', 'School history', 'Projects', 'Competencies', 'Skills']
+    }
+  }
+}
+</script>
